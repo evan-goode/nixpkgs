57d30e13ba6e0762056b06d3d69a244d03e26ddb
Evan Goode <mail@evangoo.de>
2022-01-27 01:27:16 -0500

    Update for Proccessing 4.0 beta 4

--- pkgs/applications/graphics/processing/default.nix
+++ pkgs/applications/graphics/processing/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, fetchurl, ant, unzip, makeWrapper, jdk11, javaPackages, rsync, ffmpeg, batik, gsettings-desktop-schemas, xorg, wrapGAppsHook }:
+{ lib, stdenv, fetchFromGitHub, fetchurl, ant, unzip, makeWrapper, jdk, javaPackages, rsync, ffmpeg, batik, gsettings-desktop-schemas, xorg, wrapGAppsHook }:
 let
   vaqua = fetchurl {
     name = "VAqua9.jar";
@@ -7,56 +7,42 @@ let
   };
 
   jna = fetchurl {
-    name = "jna-5.8.0.zip";
-    url = "https://github.com/java-native-access/jna/archive/5.8.0.zip";
-    sha256 = "7cRkkxyo2Ld19Nif8oWceITt7dYMUnQlrs0R5e0AmPE=";
-  };
-
-  gluon = fetchurl {
-    name = "javafx-16-sdk-linux.zip";
-    url = "https://gluonhq.com/download/javafx-16-sdk-linux";
-    sha256 = "W9Q5LQ0tak3cEr7fjZEtSvF/hiLwVg2+AoH1bE+e0Uw=";
+    name = "jna-5.10.0.zip";
+    url = "https://github.com/java-native-access/jna/archive/5.10.0.zip";
+    sha256 = "B5CakOQ8225xNsk2TMV8CbK3RcsLlb+pHzjaY5JNwg0=";
   };
 
 in
 stdenv.mkDerivation rec {
   pname = "processing";
-  version = "1276-4.0b1";
+  version = "1279-4.0b4";
 
   src = fetchFromGitHub {
     owner = "processing";
     repo = "processing4";
     rev = "processing-${version}";
-    sha256 = "9AUHT8lEkYQ+TcpHJ7D24QZf6DjTPmthd/oVg/+Pt2Q=";
+    sha256 = "G+1QEMLQZyH3l9uIGtdM5RTyF+J7F74VWGBEhC0IklM=";
   };
 
   nativeBuildInputs = [ ant unzip makeWrapper wrapGAppsHook ];
-  buildInputs = [ jdk11 javaPackages.jogl_2_3_2 ant rsync ffmpeg batik ];
+  buildInputs = [ jdk javaPackages.jogl_2_3_2 ant rsync ffmpeg batik ];
 
   dontWrapGApps = true;
 
   buildPhase = ''
     echo "tarring jdk"
-    tar --checkpoint=10000 -czf build/linux/jdk-11.0.12.tgz ${jdk11}
+    tar --checkpoint=10000 -czf build/linux/jdk-17.0.1.tgz ${jdk}
     cp ${ant}/lib/ant/lib/{ant.jar,ant-launcher.jar} app/lib/
     mkdir -p core/library
     ln -s ${javaPackages.jogl_2_3_2}/share/java/* core/library/
     cp ${vaqua} app/lib/VAqua9.jar
     unzip -qo ${jna} -d app/lib/
-    mv app/lib/{jna-5.8.0/dist/jna.jar,}
-    mv app/lib/{jna-5.8.0/dist/jna-platform.jar,}
-    GLUON="java/libraries/javafx/library"
-    mkdir -p $GLUON/linux64/modules/
-    mkdir -p $GLUON/macosx/modules/
-    unzip -qo ${gluon} -d $GLUON/
-    cp $GLUON/javafx-sdk-16/lib/*.jar $GLUON/macosx/modules/
-    cp $GLUON/javafx-sdk-16/lib/*.so $GLUON/linux64/
+    mv app/lib/{jna-5.10.0/dist/jna.jar,}
+    mv app/lib/{jna-5.10.0/dist/jna-platform.jar,}
     cp -r ${batik}/* java/libraries/svg/library/
-    cp java/libraries/svg/library/{lib/,}batik-all-1.14.jar
+    cp java/libraries/svg/library/lib/batik-all-1.14.jar java/libraries/svg/library/batik.jar
     echo "tarring ffmpeg"
     tar --checkpoint=10000 -czf build/shared/tools/MovieMaker/ffmpeg-4.4.gz ${ffmpeg}
-    sed -i 's/depends="download-javafx"//g' java/libraries/javafx/build.xml
-    sed -i '888,889d' java/libraries/javafx/src/processing/javafx/PSurfaceFX.java
     cd build
     ant build
     cd ..
@@ -66,7 +52,7 @@ stdenv.mkDerivation rec {
     mkdir $out
     cp -dpr build/linux/work $out/${pname}
     rmdir $out/${pname}/java
-    ln -s ${jdk11} $out/${pname}/java
+    ln -s ${jdk} $out/${pname}/java
     makeWrapper $out/${pname}/processing      $out/bin/processing \
       ''${gappsWrapperArgs[@]} \
       --prefix _JAVA_OPTIONS " " -Dawt.useSystemAAFontSettings=lcd
